f5853c9483766324c4bb3837f7240443dd1d7605
Salman Memon <smemon@gb-pb-dt-j06554.emea.travelex.net>
2018-10-19 17:26:14 +0530

    first commit

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+f="who am i"
+
+def main():
+	print("Hello World!")
+	f= "Who am i"
+	print(f)
+main()
+
+def whatFunction():
+	global f
+	print(f)
+
+whatFunction()
+print("Salman Memon")
---
+++ stringops.py
@@ -0,0 +1,11 @@
+var1 = "Batman"
+var2 = "Superman"
+#newstring = var2.replace('Su','Su ke')
+#print(newstring)
+print(var1.upper())
+print(var1.lower())
+print(var1.capitalize())
+print(":".join(var1))
+print(':'.join(reversed(var1)))
+print("Var1[0]:",var1[0])
+print("Var2[1:5]:",var2[1:5])
